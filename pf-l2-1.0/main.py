17b25b7f7c717a8cf903372943809bb8ac83f386
KwameBright <bright.hodogbe@genstudents.org>
2025-01-27 18:31:58 +0000

    git commit -a -m "Added pf-l2-1.0"

---
+++ pf-l2-1.0/main.py
@@ -0,0 +1,13 @@
+shopping_list = []
+#Write a program that takes the following inputs:
+#- list item 1
+#- list item 2
+#- list item 3
+item_one = input("list item 1")
+item_two = input("list item 2")
+item_three = input("list item 3")
+print(item_one)
+print(item_two)
+print(item_three)
+shopping_list =[item_one, item_two, item_three]
+print(shopping_list)
